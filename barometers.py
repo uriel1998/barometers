6cd8026e3fc6bf9f49da4066dca08b286f98f312
Steven Saus <steven@stevesaus.com>
2021-12-24 09:42:41 -0500

    More like code...

--- barometers.py
+++ barometers.py
@@ -3,16 +3,35 @@ can use a generator object to produce the various values in python by comparing 
 
 dict { epoch, date, time, pressure, ABS generated (not sure exactly how without looking), with sign generated } 
 
-
+# (optionally - look at cache file as well and read it into dict first?)
 # get locnumber from file name
+
+temperatures = []
+with open('lab_05.txt') as infile:
+    for row in infile:
+        
+        if not row.find(",,"(replace("@", ",")):
+            # okay, this is it - but this needs to be assigned to a dictionary.
+            int(split(',', row.strip()))
+            # if key time already exists in dict, skip it
+        
 # read in line by line
     # remove if empty value
-    # if key time already exists in dict, skip it
-    # (optionally - look at cache file as well and read it into dict first?)
     # sort dict keys (or read dict keys) by epoch key
-# generator for ABS_val and SIGN_val (er, research that) and add to dict
+# generator (or do calculations) for ABS_val and SIGN_val (er, research that) and add to dict
+    # you can assign functions to variables! (9.5)
+    # lambda expressions might be what I'm looking for? (9.6)
+    # nope, it's generators with a subgenerator for counting or somesuch
+        while count < 52:
+            then=get dictvalue[now-count]
+            now[position in dict for data]=now[0] - then
+            #ABSVAL as well
+            count += 1
+            
 # for last (user input?) entries, output the generated values
 # graphics to output colored data (no idea yet)
+
+
 
 list[ curr_value, [ list_of_abv[] ], [list of ] ]
 
