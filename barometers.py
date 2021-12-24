7b7b67220b60feca724527c7eb8d138f6d27522e
Steven Saus <steven@stevesaus.com>
2021-12-23 22:21:14 -0500

    pseudocode for python

---
+++ barometers.py
@@ -0,0 +1,31 @@
+
+can use a generator object to produce the various values in python by comparing positionals without having to store them in arrays holy shit
+
+dict { epoch, date, time, pressure, ABS generated (not sure exactly how without looking), with sign generated } 
+
+
+# get locnumber from file name
+# read in line by line
+    # remove if empty value
+    # if key time already exists in dict, skip it
+    # (optionally - look at cache file as well and read it into dict first?)
+    # sort dict keys (or read dict keys) by epoch key
+# generator for ABS_val and SIGN_val (er, research that) and add to dict
+# for last (user input?) entries, output the generated values
+# graphics to output colored data (no idea yet)
+
+list[ curr_value, [ list_of_abv[] ], [list of ] ]
+
+access by 
+
+list[1][3]
+
+
+New calculation is 
+
+C52-C51 C51-C50 C50-C49 C49-C48
+|(1024-1025)|+|(1025-1026)|+|(1026-1025)|+|(1025-1024)|
+1 + 1 + 1 + 1 = 4
+without abs, gets you to 0.
+
+timestamp. current pressure.  [ set of ABV(current pressure - interval back current pressure) ].  [ set of (current pressure - interval back current pressure) ]. \ 
