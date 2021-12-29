a366c20c90f3e3b8470a1d9ceafd5058bb4759f4
Steven Saus <steven@stevesaus.com>
2021-12-28 20:19:29 -0500

    Okay, rough draft of calculations code...

--- barometers.py
+++ barometers.py
@@ -1,7 +1,7 @@
 
 #can use a generator object to produce the various values in python by comparing positionals without having to store them in arrays holy shit
 # data structure
-# pressures = [ epoch, date, time, pressure, calc_sign(tuple) ] 
+# pressures = [ epoch, date, time, imp_pressure, unit, metric_pressure, unit, calc_sign(tuple) ] 
 # store with sign, calculate abs each time.
 # (optionally - look at cache file as well and read it into dict first?)
 # get locnumber from file name
@@ -17,41 +17,27 @@ def read_in_file(in_file):
         for row in infile:            
             if not row.find(",,"(replace("@", ",")):
                 # epochtime
+                # Oh, yeah, we didn't want units did we.
                 list_to_add = row.split(',',7)
                 if ( not list_to_add[1] in pressures ) AND ( not list_to_add[2] in pressures): 
                     pressures.append(list_to_add)   #needs to be a list because I will use positionals for calculations later
                     pressures.sort() # because key 0 is epochtime
     in_file.close
 
-
-# loop through pressures after value 52
-# if the tuple exists (len of the array is 7, don't calc)
-# if it doesn't exist, calc with sign.  Can ABS it later
-
-
+def perform_calculations(row):
+    """ Calculate the values for calc_signed for the specified row in pressures array """
+    global pressures
+    now = pressures[row][5]  # only need this once
+    signed_calc = [None] * 52  # maybe?
+    
+    if len(pressures[row]) is 6:   # no tuple in that row
         while count < 52:
-            then=get dictvalue[now-count]
-            now[position in dict for data]=now[0] - then
-            #ABSVAL as well
+            then = pressures[row - count][5]
+            signed_calc.append(now - then)
             count += 1
-            
+
+        pressures[row].append(tuple(signed_calc))  #This SHOULD work?
+
 # for last (user input?) entries, output the generated values
 # graphics to output colored data (no idea yet)
 
-
-
-list[ curr_value, [ list_of_abv[] ], [list of ] ]
-
-access by 
-
-list[1][3]
-
-
-New calculation is 
-
-C52-C51 C51-C50 C50-C49 C49-C48
-|(1024-1025)|+|(1025-1026)|+|(1026-1025)|+|(1025-1024)|
-1 + 1 + 1 + 1 = 4
-without abs, gets you to 0.
-
-timestamp. current pressure.  [ set of ABV(current pressure - interval back current pressure) ].  [ set of (current pressure - interval back current pressure) ]. \ 
