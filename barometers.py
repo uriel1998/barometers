a66ef83a98cf8bfdf6ad321613700c87b989b559
Steven Saus <steven@stevesaus.com>
2021-12-30 11:21:26 -0500

    Oooh

--- barometers.py
+++ barometers.py
@@ -1,4 +1,4 @@
-
+import pathlib
 #can use a generator object to produce the various values in python by comparing positionals without having to store them in arrays holy shit
 # data structure
 # pressures = [ epoch, date, time, imp_pressure, unit, metric_pressure, unit, calc_sign(tuple) ] 
@@ -7,6 +7,8 @@
 # get locnumber from file name
 
 pressures = []
+cur_path = pathlib.Path()
+
 
 # double check global and local vars - d
 
@@ -21,6 +23,9 @@ def read_in_file(in_file):
                 # Oh, yeah, we didn't want units did we.
                 list_to_add = row.split(',',7)
                 if not list_to_add[1] in pressures and not list_to_add[2] in pressures:
+                    # taking out units if they exist
+                    list_to_add.remove("hPa")
+                    list_to_add.remove("in")
                     pressures.append(list_to_add)   #needs to be a list because I will use positionals for calculations later
                     pressures.sort() # because key 0 is epochtime
     in_file.close
@@ -31,7 +36,7 @@ def perform_calculations(row):
     now = pressures[row][5]  # only need this once
     signed_calc = [None] * 52  # maybe?
     
-    if len(pressures[row]) is 6:   # no tuple in that row
+    if len(pressures[row]) == 6:   # no tuple in that row
         while count < 52:
             then = pressures[row - count][5]
             signed_calc.append(now - then)
@@ -56,8 +61,14 @@ def show_calculations(num_output):
         count += 1
 
 
-read_in_file cachefile
-read_in_file infile
+for cachefile in list(cur_path.joinpath(cur_path.cwd(),'cache').iterdir()):
+    read_in_file(cachefile)
+
+for rawfile in list(cur_path.joinpath(cur_path.cwd(),'raw').iterdir()):    
+    read_in_file(rawfile)
+
+
+
 loop_calculations
 show_calculations(52)
 
