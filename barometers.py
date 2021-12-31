35cd02f57d0f2e3896b7fce939e266b5316c15b8
Steven Saus <steven@stevesaus.com>
2021-12-31 13:59:00 -0500

    Looks like it's working both in reading and writing to cache. Next up, displaying pleasingly

--- barometers.py
+++ barometers.py
@@ -11,12 +11,13 @@ import pickle
 pressures = []
 cur_path = pathlib.Path()
 
-
-# double check global and local vars - d
+# TODO:  Max entries as passed variable
 
 def read_in_file(in_file):
     """ Reading in the new file into the pressures data structure """ 
     global pressures
+    
+    
     with open(in_file, 'r') as infile:
         for row in infile:            # DO I NEED AN EXPLICIT READLINE HERE? I don't think so...
             row = row.replace("@", ",")
@@ -30,19 +31,26 @@ def read_in_file(in_file):
                 if list_to_add[0] == "epoch":
                     continue
                 else:
-                    print("adding {0} {1} {2}".format(list_to_add[0],list_to_add[1],list_to_add[2]))
-                    # taking out units if they exist
-                    try:
-                        list_to_add.remove("hPa")
-                    except ValueError:
-                        print("already clean")
-                    
-                    try: 
-                        list_to_add.remove("in")
-                    except ValueError:
-                        print("already clean")
-                                
-                    pressures.append(list_to_add)   #needs to be a list because I will use positionals for calculations later
+                    dupe = 0
+                    for c1 in pressures:
+                        if c1[1] == list_to_add[1]:
+                            if c1[2] == list_to_add[2]:
+                                dupe = 1
+
+                    if dupe != 1:
+                        #print("adding {0} {1} {2}".format(list_to_add[0],list_to_add[1],list_to_add[2]))
+                        # taking out units if they exist
+                        try:
+                            list_to_add.remove("hPa")
+                        except ValueError:
+                            print("already clean")
+                        
+                        try: 
+                            list_to_add.remove("in")
+                        except ValueError:
+                            print("already clean")
+                                    
+                        pressures.append(list_to_add)   #needs to be a list because I will use positionals for calculations later
 
     infile.close
 
@@ -72,11 +80,10 @@ def show_calculations(num_output):
     """ Display the tuples for the requested number of intervals back """
     global pressures
     last = len(pressures)
-    count = 0
+    count = last - num_output
     while count < num_output:
-        showing = last - count
-        print (str(pressures[showing][1]) + '@' + str(pressures[showing][2]) + '@' + str(pressures[showing][5]))
-        count -= 1
+        print ("{0} @ {1} : {2}".format(count,pressures[count][2],pressures[count][5]))
+        count += 1
 
 def match_cache(weather_location):
     """ See if a pickled cache file exists for the rawfile we're reading in """
@@ -88,41 +95,32 @@ def match_cache(weather_location):
         file = open(cache_file, 'rb')
         print("Reading in cache for location {0}".format(weather_location))
         pressures = pickle.load(file)
-        file.close       
+        file.close()
     except FileNotFoundError:
         print("No cache exists for location {0}".format(cache_file))   
 
 def write_cache():
+    """ Writing pickled info to cache """
     global pressures
     global cache_file
     
+    
+    try:
+        cache_file.unlink()
+    except FileNotFoundError:
+        print ("Creating new cache {0}".format(cache_file))
+    
     file = open(cache_file, 'wb')
     pickle.dump(pressures,file)
-    file.close
+    file.close()
     
-
-# Right now I've just got one location; this will not work for when there are multiple locations. But when I get to that bridge, we can add location to the pressures list, I guess?
-
-# I guess I could also skip reading in the entire cache, and just checking 
-# the cachefile for something's existence instead of checking in pressures
-# if it just takes too horribly wrong...
-
-# would pickle be useful here for reading/writing from the cache? pg 185 
-# maybe not... since it says JSON is even quicker, so, yeah.
-
-
-# this could trip me up later because if there's multiple infiles from different locs. So....
 
 for rawfile in list(cur_path.joinpath(cur_path.cwd(),'raw').iterdir()):    
 
     weather_location = str(rawfile.name.split('_',1)[0])
     match_cache(weather_location)
-
     print("Reading in {0}".format(rawfile))
     read_in_file(rawfile)
-
-#### NEED A WAY TO CHECK WITHOUT IT BUGGERING WITH NEW CACHE FILE AND NOTHING TO COMPARE IT TO
-
 
 pressures.sort() # because key 0 is epochtime
 
@@ -131,7 +129,7 @@ write_cache()
 #writelines takes a list of strings as argument and writes them WITHOUT NEWLINES to file obj.  So maybe take the list of pressures and converts it to strings to write?
 # is it quicker to just rewrite the cache, or to check for existing values?
 
-#show_calculations(52)
+show_calculations(52)
 
 # for last (user input?) entries, output the generated values
 # graphics to output colored data (no idea yet)
