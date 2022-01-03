824e4e39612ec17755980964ead1767c9edb547b
Steven Saus <steven@stevesaus.com>
2022-01-03 03:12:53 -0500

    Multiple color schemes implemented

--- README.md
+++ README.md
@@ -10,17 +10,27 @@ cache and reports out how many records and from what dates/times.
 
 Output is controlled by command line switches.
 
-usage: barometers.py [-h] [-s NUM_OUTPUT] [-a NUM_INPUT] [-c] [-l] [-S] [-A]
-                     [-t]
+# TODO: select time period as passed interval
+# TODO: Incorporate gathering data as well?
+# TODO: Check intervals for equivalent time passage
+# Line graph auto scales!
+
+# Note on the data structure for pressures
+# pressures = epoch,date,time,pressure imperial,pressure metric, calc[64]
+
+usage: barometers.py [-h] [-d NUM_OUTPUT] [-a NUM_INPUT] [-c] [-l] [-s SCHEME]
+                     [-S] [-A] [-t]
 
 optional arguments:
   -h, --help            show this help message and exit
-  -s NUM_OUTPUT, --show-records NUM_OUTPUT
+  -d NUM_OUTPUT, --display-records NUM_OUTPUT
                         number of records back to show
   -a NUM_INPUT, --add-records NUM_INPUT
                         max number of records to add from input files
   -c, --show-calc       Show calc on stdout
   -l, --line-graph      Produce line graph overlay
+  -s SCHEME, --scheme SCHEME
+                        Color scheme - default, wide, alt, original
   -S, --signed-values   Produce signed value chart
   -A, --abs-values      Produce abs value chart
   -t, --test            Test mode: reads from stdin
--- barometers.py
+++ barometers.py
@@ -6,25 +6,38 @@ import pickle, sys, string, argparse
 from PIL import Image, ImageDraw
 
 
-# TODO: select time period as passed interval
-# TODO: Incorporate gathering data as well?
-# TODO: Check intervals for equivalent time passage
-# Line graph auto scales!
-
-# Note on the data structure for pressures
-# pressures = epoch,date,time,pressure imperial,pressure metric, calc[64]
-
-
 pressures = []
 cur_path = pathlib.Path()
-my_colors = { -16:(255, 255, 255),-15:(255, 224, 224),-14:(255, 192, 192), 
+_my_colors = { -16:(255, 255, 255),-15:(255, 224, 224),-14:(255, 192, 192), 
     -13:(255, 160, 160),-12:(255, 128, 128),-11:(255, 96, 96),  
     -10:(255, 64, 64),-9:(255, 32, 32),-8:(255, 0, 0),-7:(224, 0, 0), 
     -6:(192, 0, 0),-5:(160, 0, 0),-4:(128, 0, 0),-3:(96, 0, 0), 
-    -2:(64, 0, 0),-1:(32, 0, 0),0 : (0, 0, 0),15:(255,236,224),14:(255,216,192),
+    -2:(64, 0, 0),-1:(32, 0, 0),0:(0, 0, 0),16:(255, 255, 255),15:(255,236,224),14:(255,216,192),
     13:(255,196,160),12:(255,176,128),11:(255,156,96),10:(255,136,64),
     9:(255,116,32),8:(255,96,0),7:(224,84,0),6:(192,72,0),5:(160,60,0),
     4:(128,48,0),3:(96,36,0),2:(64,24,0),1:(32,12,0)}
+
+_my_colors_alt = { -16:(255, 255, 255),-15:(255, 224, 224),-14:(255, 192, 192), 
+    -13:(255, 160, 160),-12:(255, 128, 128),-11:(255, 96, 96),  
+    -10:(255, 64, 64),-9:(255, 32, 32),-8:(255, 0, 0),-7:(224, 0, 0), 
+    -6:(192, 0, 0),-5:(160, 0, 0),-4:(128, 0, 0),-3:(96, 0, 0), 
+    -2:(64, 0, 0),-1:(32, 0, 0),0:(0, 0, 0),16:(255, 255, 255), 15:(255, 224, 248), 
+    14:(255, 192, 240),13:(255, 160, 232), 12:(255, 128, 224), 11:(255, 96, 216),
+    10:(64, 232, 208), 9:(255, 32, 200), 8:(255, 0, 192), 7:(224, 0, 168),
+    6:(192, 0, 144), 5:(160, 0, 120), 4:(128, 0, 96), 3:(96, 0, 72),
+    2:(64, 0, 48), 1:(32, 0, 24)}
+
+_my_colors_original = { 18:(96, 0, 0),-18:(96, 0, 0), 17:(96, 12, 0), -17:(96, 24, 0), 
+    16:(96, 36, 0), -16:(96, 48, 0), 15:(96, 60, 0), -15:(96, 72, 0), 
+    14:(96, 84, 0), -14:(96, 96, 0), 13:(84, 96, 0), -13:(72, 96, 0), 
+    12:(60, 96, 0), -12:(48, 96, 0), 11:(36, 96, 0), -11:(24, 96, 0), 
+    10:(12, 96, 0), -10:(0, 96, 0), 9:(0, 96, 12), -9:(0, 96, 24), 
+    8:(0, 96, 36), -8:(0, 96, 48), 7:(0, 96, 60), -7:(0, 96, 72), 
+    6:(0, 96, 84), -6:(0, 96, 96), 5:(0, 84, 96), -5:(0, 72, 96), 
+    4:(0, 60, 96), -4:(0, 48, 96), 3:(0, 36, 96), -3:(0, 24, 96), 
+    2:(0, 12, 96), -2:(0, 0, 96), 1:(0, 0, 64), -1:(0, 0, 32), 0:(0, 0, 0)}
+
+_my_colors_wide = {-24:(192, 0, 0), -23:(192, 24, 0), -22:(192, 48, 0), -21:(192, 72, 0), -20:(192, 96, 0), -19:(192, 120, 0), -18:(192, 144, 0), -17:(192, 168, 0), -16:(192, 192, 0), -15:(168, 192, 0), -14:(144, 192, 0), -13:(120, 192, 0), -12:(96, 192, 0), -11:(72, 192, 0), -10:(48, 192, 0), -9:(24, 192, 0), -8:(0, 192, 0), -7:(0, 192, 24), -6:(0, 192, 48), -5:(0, 192, 72), -4:(0, 192, 96), -3:(0, 192, 120), -2:(0, 192, 144), -1:(0, 192, 168), 0:(0, 192, 192), 1:(0, 168, 192), 2:(0, 144, 192), 3:(0, 120, 192), 4:(0, 96, 192), 5:(0, 72, 192), 6:(0, 48, 192), 7:(0, 24, 192), 8:(0, 0, 192), 9:(24, 0, 192), 10:(48, 0, 192), 11:(72, 0, 192), 12:(96, 0, 192), 13:(120, 0, 192), 14:(144, 0, 192), 15:(168, 0, 192), 16:(192, 0, 192), 17:(192, 0, 168), 18:(192, 0, 144), 19:(192, 0, 120), 20:(192, 0, 96), 21:(192, 0, 72), 22:(192, 0, 48), 23:(192, 0, 24), 24:(192, 0, 0)}
 
 
 
@@ -150,11 +163,9 @@ def match_cache(weather_location):
     except FileNotFoundError:
         print("No cache exists for location {0}".format(cache_file))   
 
-def make_chart(num_output = 64, linegraph = False):
+def make_chart(num_output = 64, linegraph = False, scheme = None):
     """ Create output graphic chart with signed data """
     global pressures
-    global my_colors
-    
     
     my_image = Image.new('RGB', (552, (num_output * 8)), (125, 125, 125))
     draw = ImageDraw.Draw(my_image)
@@ -164,7 +175,15 @@ def make_chart(num_output = 64, linegraph = False):
     while count < len(pressures):  # row count        
         x_counter = 0
         while x_counter < 64:
-            fill_color = my_colors.get(pressures[count][5][x_counter], (255, 255, 255))           
+            if scheme == "wide":
+                fill_color = _my_colors_wide.get(pressures[count][5][x_counter], (255, 255, 255))
+            elif scheme == "alt":
+                fill_color = _my_colors_alt.get(pressures[count][5][x_counter], (255, 255, 255))
+            elif scheme == "original":
+                fill_color = _my_colors_original.get(pressures[count][5][x_counter], (255, 255, 255))
+            else:
+                fill_color = _my_colors.get(pressures[count][5][x_counter], (255, 255, 255))           
+            
             x = (x_counter * 8) + 49
             #print ("x = {0} y = {1} color = {2}".format(x,y,fill_color))
             draw.rectangle((x, y, x + 8, y + 8), fill=(fill_color) , outline=None)
@@ -181,10 +200,9 @@ def make_chart(num_output = 64, linegraph = False):
     my_image.save('signed_color.png')
 
 
-def make_abs_chart(num_output = 64,linegraph = False):
+def make_abs_chart(num_output = 64,linegraph = False,scheme = None):
     """ Create output graphic chart with ABS data """
     global pressures
-    global my_colors
     
     my_image = Image.new('RGB', (552, (num_output * 8)), (125, 125, 125))
     draw = ImageDraw.Draw(my_image)
@@ -194,7 +212,15 @@ def make_abs_chart(num_output = 64,linegraph = False):
     while count < len(pressures):  # row count        
         x_counter = 0
         while x_counter < 64:
-            fill_color = my_colors.get(abs(int(pressures[count][5][x_counter])), (255, 255, 255))
+            if scheme == "wide":
+                fill_color = _my_colors_wide.get(abs(pressures[count][5][x_counter]), (254, 255, 255))
+            elif scheme == "alt":
+                fill_color = _my_colors_alt.get(abs(pressures[count][5][x_counter]), (255, 255, 255))
+            elif scheme == "original":
+                fill_color = _my_colors_original.get(abs(pressures[count][5][x_counter]), (255, 255, 255))
+            else:
+                fill_color = _my_colors.get(1-(abs(pressures[count][5][x_counter])), (255, 255, 255))           
+
             x = (x_counter * 8) + 40
             #print ("x = {0} y = {1} color = {2}".format(x,y,fill_color))
             draw.rectangle((x, y, x + 8, y + 8), fill=(fill_color) , outline=None)
@@ -213,8 +239,7 @@ def make_abs_chart(num_output = 64,linegraph = False):
 def write_cache():
     """ Writing pickled info to cache """
     global pressures
-    global cache_file
-    
+    global cache_file    
     
     try:
         cache_file.unlink()
@@ -229,10 +254,11 @@ def main():
     """ main loop """
     
     parser = argparse.ArgumentParser(usage=__doc__)
-    parser.add_argument("-s", "--show-records", type=int, help="number of records back to show", default=64,action='store',dest='num_output')
+    parser.add_argument("-d", "--display-records", type=int, help="number of records back to show", default=64,action='store',dest='num_output')
     parser.add_argument("-a", "--add-records", type=int, help="max number of records to add from input files", default=256,action='store',dest='num_input')
     parser.add_argument("-c", "--show-calc", dest="showcalc",action='store_true', default=False, help="Show calc on stdout")
     parser.add_argument("-l", "--line-graph", dest="linegraph",action='store_true', default=False, help="Produce line graph overlay")
+    parser.add_argument("-s", "--scheme", dest="scheme",action='store', default=None, help="Color scheme - default, wide, alt, original")
     parser.add_argument("-S", "--signed-values", dest="signval",action='store_true', default=False, help="Produce signed value chart")
     parser.add_argument("-A", "--abs-values", dest="absval",action='store_true', default=False, help="Produce abs value chart")
     parser.add_argument("-t", "--test", dest="test",action='store_true', default=False, help="Test mode: reads from stdin")
@@ -262,9 +288,9 @@ def main():
         if args.showcalc:
             show_calculations(args.num_output)
         if args.signval:
-            make_chart(args.num_output,args.linegraph)
+            make_chart(args.num_output,args.linegraph,args.scheme)
         if args.absval:
-            make_abs_chart(args.num_output,args.linegraph)
+            make_abs_chart(args.num_output,args.linegraph,args.scheme)
 
 
 if __name__ == '__main__':
